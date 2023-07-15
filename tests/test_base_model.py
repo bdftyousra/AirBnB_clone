5997d3e07911e8be513aa06fae38d6f9b27753ae
bdftyousra@gmail.com <bdftyousra@gmail.com>
2023-07-15 20:34:29 +0000

    file changed

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import os
-import sys
-from models.base_model import BaseModel
-
-def main():
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-if __name__ == "__main__":
-    main()
-=======
 #!/usr/bin/python3
 """Defines unittests for models/base_model.py.
 Unittest classes:
@@ -210,5 +189,3 @@ class TestBaseModel_to_dict(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
->>>>>>> 0bfc1bfd739ddaed6071219862e80369faeb4dd8
-
